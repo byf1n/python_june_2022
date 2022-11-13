0068c29e4588b71bd3c99f275dd61625c6efd34c
byf1n <kirillozubenko@gmail.com>
2022-11-13 20:14:34 +0100

    Initial commit

---
+++ lesson1/lesson1.py
@@ -0,0 +1,13 @@
+# print('hello world')
+# print(1,2,3,4,sep='-',end='')
+# print(12121)
+
+# print(isinstance('23',int))
+
+user = {
+    "name": 'max',
+     'age': 15
+}
+for k,v in user.items():
+    print(f'{k = }')
+    print(f'{v = }')
